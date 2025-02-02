30a584b8c0621b01e45f023e28a66cc2bc0b54c4
ppablooz74 <ppablooz74@gmail.com>
2025-02-02 19:28:53 +0000

    feat: improve search and layout of potions

--- src/components/PotionsList/PotionsList.jsx
+++ src/components/PotionsList/PotionsList.jsx
@@ -1,10 +1,25 @@
-import { useState, useEffect } from "react"
-import potionsData from "../../json/potions.json"
-import "./PotionsList.css"
+import { useState, useEffect } from "react";
+import potionsData from "../../json/potions.json";
+import "./PotionsList.css";
 
 function PotionList() {
-  const { pociones } = potionsData
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+    function removeAccents(str) {
+        return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+      }
+
+      function compareStringsWithoutAccents(str1, str2) {
+        return removeAccents(str1.toLowerCase()).startsWith(removeAccents(str2.toLowerCase()))
+      }
+
+  const { potions } = potionsData;
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredPotions, setFilteredPotions] = useState(potions);
+
+  const handleSearchChange = (event) => {
+    const newSearchTerm = event.target.value; 
+    setSearchTerm(newSearchTerm);
+  };
 
   useEffect(() => {
     const handleResize = () => setWindowWidth(window.innerWidth)
@@ -12,52 +27,66 @@ function PotionList() {
     return () => window.removeEventListener("resize", handleResize)
   }, [])
 
-  const isLandscape = windowWidth > 768
+  useEffect(() => {
+    const filtered = potions.filter((potion) =>   
+      compareStringsWithoutAccents(potion.name, searchTerm)
+    );
+    setFilteredPotions(filtered);
+  }, [searchTerm]);
 
+  const isLandscape = windowWidth > 768;
   return (
-    <div className="container">
+    <div className="potion-container">
+      <input
+        type="text"
+        placeholder="Buscar poción"
+        value={searchTerm}
+        onChange={handleSearchChange}
+        className="search-bar"
+      />
       <div className="potion-list-container">
-        {pociones.map((potion) => {
-          const { nombre, efecto, duración, nivel, tipo, ingredientes } = potion
+        {filteredPotions.map((potion) => {
+          const { name, effect, duration, level, type, ingredients } = potion;
           return (
-            <div key={nombre} className="potion-card">
-              <h3 className="potion-name">{nombre}</h3>
+            <div key={name} className="potion-card">
+              <h3 className="potion-name">{name}</h3>
               <div className="potion-card-content">
                 <div className={`extra-info ${isLandscape ? "landscape" : ""}`}>
-                  <p>
-                    <strong>Tipo:</strong> {tipo}
-                  </p>
-                  <p>
-                    <strong>Nivel:</strong> {nivel}
-                  </p>
-                  <p>
-                    <strong>Duración:</strong> {duración}
-                  </p>
+                  <div className="key-value-container">
+                    <p>
+                      <strong>Tipo</strong>
+                    </p>
+                    <p>{type}</p>
+                  </div>
+                  <div className="key-value-container">
+                    <p>
+                      <strong>Nivel</strong>
+                    </p>
+                    <p>{level}</p>
+                  </div>
+                  <div className="key-value-container">
+                    <p><strong>Duración</strong> </p>
+                    <p>{duration}</p>
+                  </div>
                 </div>
-                <div className="effect-container">
+                 <div className="effect-container">
                   <p className="effect">
-                    <strong>Efecto:</strong> {efecto}
+                    <strong>Efecto:</strong> {effect}
                   </p>
                 </div>
-                <div className="ingredientes-container">
+                <div className="ingredients-container">
                   <h4>Ingredientes:</h4>
                   <div className="ingredient-list">
-                    {ingredientes.map(({ nombre, cantidad }, index) => (
-                      <div key={index} className="ingredient-row">
-                        <span className="ingredient-amount">{cantidad}</span>
-                        <span className="ingredient-name">{nombre}</span>
-                      </div>
-                    ))}
+                    {ingredients.map((ingredient, index) => <div key={index} className="ingredient-row"> <span className="ingredient-name">{ingredient}</span></div>)}
                   </div>
                 </div>
               </div>
             </div>
-          )
+          );
         })}
       </div>
     </div>
-  )
+  );
 }
 
-export default PotionList
-
+export default PotionList;
