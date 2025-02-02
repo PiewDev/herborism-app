3f7426c588ce48b6e99829ad0fa8f4211dec14da
ppablooz74 <ppablooz74@gmail.com>
2025-02-02 17:40:43 +0000

    feat: center potion list and adjust card size

--- src/components/PotionsList/PotionsList.jsx
+++ src/components/PotionsList/PotionsList.jsx
@@ -1,40 +1,63 @@
-import React from 'react';
-import potionsData from '../../json/potions.json';
-import './PotionsList.css';
-
-
+import { useState, useEffect } from "react"
+import potionsData from "../../json/potions.json"
+import "./PotionsList.css"
 
 function PotionList() {
-    const { pociones } = potionsData;
-    
-    return (
-        <div className="potion-list-container">
-            {pociones.map((potion) => {
-                const { nombre, efecto, duracion, nivel, tipo, ingredientes } = potion;
-                return (
-                    <div key={nombre} className="potion-card">
-                        <h3 className="potion-name">{nombre}</h3>
-                        <div className="potion-details">
-                            <p><strong>Efecto:</strong> {efecto}</p>
-                            <p><strong>Duración:</strong> {duracion}</p>
-                            <p><strong>Nivel:</strong> {nivel}</p>
-                            <p><strong>Tipo:</strong> {tipo}</p>
-                            <div className='ingredientes-container'>
-                              <h4>Ingredientes:</h4>
-                                <div className="ingredient-list">
-                                    {ingredientes.map(({ nombre, cantidad }, index) => (
-                                        <p key={index} className="ingredient-row">
-                                            {cantidad} - {nombre}
-                                        </p>
-                                    ))}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                );
-            })}
-        </div>
-    );
+  const { pociones } = potionsData
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth)
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
+
+  const isLandscape = windowWidth > 768
+
+  return (
+    <div className="container">
+      <div className="potion-list-container">
+        {pociones.map((potion) => {
+          const { nombre, efecto, duración, nivel, tipo, ingredientes } = potion
+          return (
+            <div key={nombre} className="potion-card">
+              <h3 className="potion-name">{nombre}</h3>
+              <div className="potion-card-content">
+                <div className={`extra-info ${isLandscape ? "landscape" : ""}`}>
+                  <p>
+                    <strong>Tipo:</strong> {tipo}
+                  </p>
+                  <p>
+                    <strong>Nivel:</strong> {nivel}
+                  </p>
+                  <p>
+                    <strong>Duración:</strong> {duración}
+                  </p>
+                </div>
+                <div className="effect-container">
+                  <p className="effect">
+                    <strong>Efecto:</strong> {efecto}
+                  </p>
+                </div>
+                <div className="ingredientes-container">
+                  <h4>Ingredientes:</h4>
+                  <div className="ingredient-list">
+                    {ingredientes.map(({ nombre, cantidad }, index) => (
+                      <div key={index} className="ingredient-row">
+                        <span className="ingredient-amount">{cantidad}</span>
+                        <span className="ingredient-name">{nombre}</span>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
-export default PotionList;
+export default PotionList
+
